48b17b2198b86a063f8a2252663eff34b5e66772
SebastianMaluk <samaluk@miuandes.cl>
2021-03-22 16:51:39 -0300

    Update Lab1.py
    First try, all steps done (not tested)

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,7 @@
 import random
 
 #n = int(input("Cuantas cartas van a jugar?"))
-n = 8
+n = 50
 cards = []
 for i in range(1, n+1):
     cards.append(i)
@@ -30,22 +30,81 @@ for i in ast:
     print(*i)
 
 x = True
-while x:
-    print("Juega J1")
-    vuelta = "1, 1" #str(input("Qué carta quiere dar vuelta? fila, columna\n"))
-    pos = vuelta.split(", ")
-    pos[0] = int(pos[0])
-    pos[1] = int(pos[1])
-    print(pos)
-    ast[pos[0]][pos[1]] = tablero[pos[0]][pos[1]]
+T = 1
+while x == True:
+    if T == 1:
+        print("Juega J1")
+        P1_old = P1
+        vuelta = "1, 1" #str(input("Qué carta quiere dar vuelta? fila, columna\n"))
+        pos = vuelta.split(", ")
+        pos[0] = int(pos[0])
+        pos[1] = int(pos[1])
+        print(pos)
+        ast[pos[0]][pos[1]] = tablero[pos[0]][pos[1]]
+        for i in ast:
+            print(*i)
+        print("")
+        vuelta2 = "2, 2" #str(input("Dónde está su par? fila, columna\n"))
+        pos2 = vuelta2.split(", ")
+        pos2[0] = int(pos2[0])
+        pos2[1] = int(pos2[1])
+        ast[pos2[0]][pos2[1]] = tablero[pos2[0]][pos2[1]]
+        for i in ast:
+            print(*i)
+        if ast[pos[0]][pos[1]] == ast[pos2[0]][pos2[1]]:
+            print("Correcto! Encontraste un par.")
+            P1 += 1
+            ast[pos[0]][pos[1]] = " "
+            ast[pos2[0]][pos2[1]] = " "
+        else:
+            print("Mala suerte :( Sigue intentando")
+            ast[pos[0]][pos[1]] = "*"
+            ast[pos2[0]][pos2[1]] = "*"
+        if P1 == P1_old:
+            T = 2
+        if P1 + P2 == 50:
+            break
+    if T == 2:
+        print("Juega J2")
+        P2_old = P2
+        vuelta = str(input("Qué carta quiere dar vuelta? fila, columna\n"))
+        pos = vuelta.split(", ")
+        pos[0] = int(pos[0])
+        pos[1] = int(pos[1])
+        print(pos)
+        ast[pos[0]][pos[1]] = tablero[pos[0]][pos[1]]
+        for i in ast:
+            print(*i)
+        print("")
+        vuelta2 = str(input("Dónde está su par? fila, columna\n"))
+        pos2 = vuelta2.split(", ")
+        pos2[0] = int(pos2[0])
+        pos2[1] = int(pos2[1])
+        ast[pos2[0]][pos2[1]] = tablero[pos2[0]][pos2[1]]
+        for i in ast:
+            print(*i)
+        if ast[pos[0]][pos[1]] == ast[pos2[0]][pos2[1]]:
+            print("Correcto! Encontraste un par.")
+            P2 += 1
+            ast[pos[0]][pos[1]] = " "
+            ast[pos2[0]][pos2[1]] = " "
+        else:
+            print("Mala suerte :( Sigue intentando")
+            ast[pos[0]][pos[1]] = "*"
+            ast[pos2[0]][pos2[1]] = "*"
+        if P2 == P2_old:
+            T = 1
+        if P1 + P2 == 50:
+            break
     for i in ast:
         print(*i)
-    print("")
-    vuelta2 = "2, 2" #str(input("Dónde está su par? fila, columna\n"))
-    pos2 = vuelta2.split(", ")
-    pos2[0] = int(pos2[0])
-    pos2[1] = int(pos2[1])
-    ast[pos2[0]][pos2[1]] = tablero[pos2[0]][pos2[1]]
-    for i in ast:
-        print(*i)
+    
+    
     x = False
+print(f"J1: {P1}\nJ2: {P2}")
+if P1 > P2:
+    print("Ganó el jugador 1")
+elif P1 < P2:
+    print("Ganó el jugador 2")
+else:
+    print("Empate")
